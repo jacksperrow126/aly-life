d3fa32c066fcbfc6d86e10d7e5ad3f8a60ec6d6c
lamhetminh <jacksperrow12699@gmail.com>
2021-05-26 12:44:56 +0700

    Update data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
   {
     path: 'home',
     loadChildren: () => import('./view/pages/home/home.module').then(m => m.HomePageModule)
