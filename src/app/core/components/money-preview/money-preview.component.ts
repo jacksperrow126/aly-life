d7c02c8398cb581d183beb55c578a0e7772ad068
Nguyen Thanh An <an.nguyen@techx.vn>
2021-06-27 01:02:33 +0700

    Add 2 chart in preview

--- src/app/core/components/money-preview/money-preview.component.ts
+++ src/app/core/components/money-preview/money-preview.component.ts
@@ -1,4 +1,3 @@
-import { moneyIncomeType, moneyOutcomeType } from '@core/data/money';
 import { Component, OnInit } from '@angular/core';
 import { MoneyService } from '@core/services/money.service';
 
@@ -11,14 +10,14 @@ export class MoneyPreviewComponent implements OnInit {
   public currentBalance: number;
   public loan: number;
 
-  public chartData: any[];
+  public loanChartData: any[];
   constructor(private moneyService: MoneyService) {}
 
   ngOnInit() {
     this.moneyService.initMoneyService.subscribe((data) => {
       this.currentBalance = this.moneyService.getCurrentBalance();
       this.loan = this.moneyService.getCurrentLoan();
-      this.chartData = [
+      this.loanChartData = [
         { name: 'Tiền', y: this.currentBalance },
         { name: 'Nợ', y: this.loan },
       ];
